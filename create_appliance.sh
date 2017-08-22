7ae755218060c4ecfdd6e6dca81314253a2778bf
Antônio Vinícius Menezes Medeiros <vinyanalista@gmail.com>
2017-08-22 18:57:12 -0300

    Some applications added and some removed

--- create_appliance.sh
+++ create_appliance.sh
@@ -9,7 +9,7 @@
 # Contact: feedback@susestudio.com
 # ============================================================================
 
-image_file='image/LiveSUSE_Leap_42.3_KDE.x86_64-0.0.2.iso'
+image_file='image/LiveSUSE_Leap_42.3_KDE.x86_64-0.0.4.iso'
 image_arch='x86_64'
 schema_ver='5.2'
 base_system='Leap_42.1'
